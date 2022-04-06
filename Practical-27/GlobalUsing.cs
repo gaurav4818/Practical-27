8203afcc0bc4f6fc6a8a01f760483782ed5e321b
Gaurav Rathor <rathorgaurav4818@gmail.com>
2022-04-06 12:19:22 +0530

    Add project files.

---
+++ Practical-27/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿//c#10 feature -- Global using directive
+
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
